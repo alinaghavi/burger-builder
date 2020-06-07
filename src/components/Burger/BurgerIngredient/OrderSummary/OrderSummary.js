a7bf942f8bf9bc3f9655fb7127f8120ce4d5987c
Ali Naghavi <naghavi_a@irtoyaco.com>
2020-06-07 16:53:48 +0430

    developing personal button

--- src/components/Burger/BurgerIngredient/OrderSummary/OrderSummary.js
+++ src/components/Burger/BurgerIngredient/OrderSummary/OrderSummary.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React from "react";
 
 import Auxiliary from "../../../../hoc/Auxillary";
 
-import styles from './OrderSummary.module.css';
+import styles from "./OrderSummary.module.css";
+import Button from "../../../UI/Button/Button";
 
 const OrderSummary = (props) => {
-    const ingredientSummery = Object.keys(props.ingredients)
-            .map(igKey => {
-                return <li key={igKey} style={{textTransform: 'capitalize'}}>
-                    <span>{igKey}</span>
-                    :
-                    {props.ingredients[igKey]}
-                </li>
-            });
+  const ingredientSummery = Object.keys(props.ingredients).map((igKey) => {
     return (
-        <Auxiliary>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following ingredients: </p>
-            <ul>
-                {ingredientSummery}
-            </ul>
-            <p>Continue to checkout? </p>
-        </Auxiliary>
+      <li key={igKey} style={{ textTransform: "capitalize" }}>
+        <span>{igKey}</span>:{props.ingredients[igKey]}
+      </li>
     );
+  });
+  return (
+    <Auxiliary>
+      <h3>Your Order</h3>
+      <p>A delicious burger with following ingredients: </p>
+      <ul>{ingredientSummery}</ul>
+      <p>Continue to checkout? </p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Auxiliary>
+  );
 };
 
 export default OrderSummary;
